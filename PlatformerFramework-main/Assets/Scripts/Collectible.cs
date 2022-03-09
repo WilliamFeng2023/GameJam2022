ecd4d83e5e95e7928b4446ece749080ecf44cae4
WilliamFeng2023 <90072993+WilliamFeng2023@users.noreply.github.com>
2022-03-08 17:17:26 -0800

    trophie

--- PlatformerFramework-main/Assets/Scripts/Collectible.cs
+++ PlatformerFramework-main/Assets/Scripts/Collectible.cs
@@ -9,11 +9,22 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public bool pickedUpAlive = false;
+    public bool pickedUpGhost = false;
+
     public int points = 10;
+    public int x = 7;
 
     public AudioClip PickUpNoise;
 
     public GameObject SpawnOnPickUp;
+
+    public GameObject AliveTwin;
+    public GameObject GhostTwin;
+
+ 
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +35,25 @@ public class Collectible : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(pickedUpAlive == true)
+        {
+            float AliveXPos = AliveTwin.transform.position.x;
+            float AliveYPos = AliveTwin.transform.position.y;
+            Vector3 offset = new Vector3(0.0f, AliveYPos + 0.2f, 0.0f);
+            transform.position = GhostTwin.transform.position;
+        }
+        if (pickedUpGhost == true)
+        {
+            float GhostXPos = GhostTwin.transform.position.x;
+            float GhostYPos = GhostTwin.transform.position.y;
+            Vector3 offset = new Vector3(0.0f, GhostYPos + 0.2f, 0.0f);
+            transform.position = GhostTwin.transform.position;
+        }
+    }
+
+    public float getXAxis()
+    {
+        return transform.position.x;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,7 +70,11 @@ public class Collectible : MonoBehaviour
             {
                 Instantiate(SpawnOnPickUp, transform.position, transform.rotation);
             }
-            Destroy(gameObject);
+            pickedUpAlive = true;
+        }
+        else if(collision.gameObject.CompareTag("Player2"))
+        {
+            pickedUpGhost = true;
         }
     }
 }
